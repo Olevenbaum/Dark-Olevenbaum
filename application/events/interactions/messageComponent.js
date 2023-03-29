e6ada76c779d995231f3eaea165ab7b71a70b5d3
Olevenbaum <88549096+Olevenbaum@users.noreply.github.com>
2023-03-29 20:55:02 +0200

    starting with message component handler

--- application/events/interactions/messageComponent.js
+++ application/events/interactions/messageComponent.js
@@ -1,9 +1,27 @@
+// Importing modules
+const fs = require("node:fs");
+const path = require("node:path");
+
 // Importing classes
-const { InteractionType } = require("discord.js");
+const { Collection } = require("discord.js");
+
+// Reading message component types
+const messageComponentTypes = new Collection();
+const messageComponentsPath = path.join(__dirname, "./messageComponents");
+const messageComponentFiles = fs
+    .readdirSync(messageComponentsPath)
+    .filter((file) => file.endsWith(".js"));
+for (const file of messageComponentFiles) {
+    const messageComponentType = require(path.join(
+        messageComponentsPath,
+        file
+    ));
+    messageComponentTypes.set(messageComponentType.name, messageComponentType);
+}
 
 module.exports = {
-    // Setting interaction type name
-    name: InteractionType.MessageComponent,
+    // Setting message component type name
+    name: messageComponentType,
 
     // Handling interaction
     async execute(interaction) {},
