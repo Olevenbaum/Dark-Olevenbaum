37f13836a7871cc0d99baf4a115b402b4c11e076
Olevenbaum <88549096+Olevenbaum@users.noreply.github.com>
2023-03-29 20:50:32 +0200

    adding messageComponent interaction

---
+++ application/events/interactions/messageComponent.js
@@ -0,0 +1,10 @@
+// Importing classes
+const { InteractionType } = require("discord.js");
+
+module.exports = {
+    // Setting interaction type name
+    name: InteractionType.MessageComponent,
+
+    // Handling interaction
+    async execute(interaction) {},
+};
