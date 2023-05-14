988f43d5dcc47d6b7041a45e66fff327b5171a66
Olevenbaum <88549096+Olevenbaum@users.noreply.github.com>
2023-05-14 17:49:59 +0200

    adding config imports to application commands

--- application/events/interactions/applicationCommands/user.js
+++ application/events/interactions/applicationCommands/user.js
@@ -1,6 +1,9 @@
 // Importing classes and methods
 const { ApplicationCommandType } = require("discord.js");
 
+// Importing configuration data
+const { consoleSpace } = require("../../../../configuration.json");
+
 module.exports = {
     // Setting interaction type name
     name: ApplicationCommandType.User,
